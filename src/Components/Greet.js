5432f1d719d5a74ad86df81a7a96612786be1500
P Mule <sneha.p-mule@capgemini.com>
2020-09-22 19:45:29 +0530

    greet

--- src/Components/Greet.js
+++ src/Components/Greet.js
@@ -2,5 +2,5 @@ import React from 'react'
 // function Greet (){
 //     return <h1>Hello Sneha</h1>
 // }
-export const greet= () =><h1>Hello Sneha</h1>
+export const Greet= () =><h1>Hello Sneha</h1>
 // export default Greet
