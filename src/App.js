81d885e5abdcb190c00d9de2e62c9a9bd6584afb
P Mule <sneha.p-mule@capgemini.com>
2020-09-22 19:18:51 +0530

    New classes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./styles.css";
-
+import Greet from './Components'
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      {/* <h1>Hello World</h1> */}
+      <Greet></Greet>
     </div>
   );
 }
---
+++ src/Components/Greet.js
@@ -0,0 +1,6 @@
+import React from 'react'
+// function Greet (){
+//     return <h1>Hello Sneha</h1>
+// }
+export const greet= () =><h1>Hello Sneha</h1>
+// export default Greet
