a6b0d86a1c885594bc621036ed25e969ad7f9167
P Mule <sneha.p-mule@capgemini.com>
2020-09-23 14:11:37 +0530

    Class comps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./styles.css";
-import Greet from './Components'
+import Greet from './Components/Greet'
+import Welcome from './Components/Welcome'
 export default function App() {
   return (
     <div className="App">
       {/* <h1>Hello World</h1> */}
       <Greet></Greet>
+      <Welcome />
     </div>
   );
 }
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,1 +1,7 @@
 import React, {Component} from 'react'
+class Welcome extends Component{
+    render(){
+        return <h1>Welcome to class components</h1>
+    }
+}
+export default Welcome
