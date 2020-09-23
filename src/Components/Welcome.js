216e329a56ddc9764b069ab5f99cb7fb05943b27
Sneha0992 <33449457+Sneha0992@users.noreply.github.com>
2020-09-23 14:15:34 +0530

    Change class Change class

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react'
-class Welcome extends Component{
-    render(){
-        return <h1>Welcome to class components</h1>
-    }
+import React, { Component } from "react";
+class Welcome extends Component {
+  render() {
+    return <h1>Welcome to class components</h1>;
+  }
 }
-export default Welcome
+export default Welcome;
